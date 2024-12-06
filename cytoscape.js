de4075bfaf44dca10a76eee818dd9bbde82571f2
Christopher Dignam <dignachr@gmail.com>
2024-12-06 11:34:54 +0100

    Fix bug in js file

--- cytoscape.js
+++ cytoscape.js
@@ -1,32 +1,30 @@
-<script>
-    fetch('graph-data.json')
-        .then(response => response.json())
-        .then(data => {
-            var cy = cytoscape({
-                container: document.getElementById('cy'),
-                elements: data.elements,
-                style: [
-                    {
-                        selector: 'node',
-                        style: {
-                            'background-color': '#0074D9',
-                            'label': 'data(label)'
-                        }
-                    },
-                    {
-                        selector: 'edge',
-                        style: {
-                            'width': 2,
-                            'line-color': '#B10DC9',
-                            'target-arrow-color': '#B10DC9',
-                            'target-arrow-shape': 'triangle',
-                            'label': 'data(label)'
-                        }
+fetch('graph-data.json')
+    .then(response => response.json())
+    .then(data => {
+        var cy = cytoscape({
+            container: document.getElementById('cy'),
+            elements: data.elements,
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': '#0074D9',
+                        'label': 'data(label)'
                     }
-                ],
-                layout: {
-                    name: 'cose'
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 2,
+                        'line-color': '#B10DC9',
+                        'target-arrow-color': '#B10DC9',
+                        'target-arrow-shape': 'triangle',
+                        'label': 'data(label)'
+                    }
                 }
-            });
+            ],
+            layout: {
+                name: 'cose'
+            }
         });
-</script>
+    });
